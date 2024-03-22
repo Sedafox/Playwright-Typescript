7d7e6e8cd4a5554a2275f06c79b05fb674870456
usagimadokii@gmail.com <usagimadokii@gmail.com>
2024-03-21 23:13:24 -0500

    - Added signInAsUser fixture to set up for tests with various users

--- fixtures.ts
+++ fixtures.ts
@@ -1,9 +1,11 @@
 import { InventoryPage } from "./Pages/InventoryPage";
 import { LoginPage } from "./Pages/LoginPage";
 import { test as base, Page } from "@playwright/test";
+import { USER_ROLES, getUsername, getPassword } from "./users";
 
 export const test = base.extend<{
   pages: { loginPage: LoginPage; inventoryPage: InventoryPage };
+  signInAsUser: (user: USER_ROLES) => Promise<void>;
 }>({
   pages: async ({ page }: { page: Page }, use) => {
     const pages = {
@@ -11,5 +13,12 @@ export const test = base.extend<{
       inventoryPage: new InventoryPage(page),
     };
     await use(pages);
+  },
+
+  signInAsUser: async ({ pages }, use) => {
+    const signInAsUser = async (user: USER_ROLES) => {
+      await pages.loginPage.login(getUsername(user), getPassword(user));
+    };
+    await use(signInAsUser);
   },
 });
